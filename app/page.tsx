88115f3da3e8a7f917ce27f2daf773ad80ead9ba
adolfo <adolfo@makina.home.local>
2025-03-14 19:08:44 +0100

    new day

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default async function Home() {
   const user = await checkUser();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to your App</h1>
-      <Button><a href="/sign-in">SignIn Now</a></Button>
+      <h1 className="text-4xl font-bold text-center">Bienvenue<br />LAURA<br />&<br />CLARA</h1>
+      <Button><a href="/sign-in">Sign-In</a></Button>
       {user?.role === "admin" && (
               <Link href="/admin/dashboard">
                 <Button variant="secondary" className="bg-blue-600 hover:bg-blue-700 text-white">
